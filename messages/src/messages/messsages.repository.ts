593a47458f92fa1d1a686297d732be22aaa01b43
SY <12tndbs12@naver.com>
2021-09-28 16:30:39 +0900

    저장파일 읽기 및 쓰기

--- messages/src/messages/messsages.repository.ts
+++ messages/src/messages/messsages.repository.ts
@@ -10,10 +10,19 @@ export class MessagesRepository {
   }
 
   async findAll() {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
 
+    return messages;
   }
 
-  async create(message: string) {
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
 
+    const id = Math.floor(Math.random() * 999)
+    messages[id] = { id, content };
+
+    await writeFile('messages.json', JSON.stringify(messages));
   }
 }
