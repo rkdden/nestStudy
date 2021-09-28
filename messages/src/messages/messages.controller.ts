e6fb9e1e873f68674af8b33868154f8f37ea9d6e
SY <12tndbs12@naver.com>
2021-09-28 16:44:58 +0900

    컨트롤러 수동 테스트

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,18 +1,25 @@
+import { MessagesService } from './messages.service';
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDTO } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
   createMessage(@Body() body: CreateMessageDTO) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log(id);
+    return this.messagesService.findOne(id);
   }
 }
