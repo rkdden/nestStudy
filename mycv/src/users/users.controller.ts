db4f852ba0afefe7ecc1612fd03655c620dfd1e4
SY <12tndbs12@naver.com>
2021-10-09 21:24:33 +0900

    레코드 찾기 및 필터링

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -1,5 +1,5 @@
 import { UsersService } from './users.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create-user.dto';
 
 @Controller('auth')
@@ -10,5 +10,15 @@ export class UsersController {
   @Post('/signup')
   createUser(@Body() body: CreateUserDTO) {
     this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
   }
 }
