65a5111679f60c7ce0c44967697011bcaafef717
SY <12tndbs12@naver.com>
2021-10-13 15:13:25 +0900

    인터셉터의 직렬화

--- mycv/src/interceptors/serialize.interceptor.ts
+++ mycv/src/interceptors/serialize.interceptor.ts
@@ -1,15 +1,20 @@
+import { UserDto } from './../users/dtos/user.dto';
 import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
+import { plainToClass } from "class-transformer";
 import { map, Observable } from "rxjs";
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
     // 요청이 처리되기 전에 실행될 구문
-    console.log('Im running before the handler', context);
+    // console.log('Im running before the handler', context);
     
     return handler.handle().pipe(
       map((data: any) => {
         // 요청이 처리된 후에 전달되기 전에 실행될 구문
-        console.log('Im running before response is sent out', data);
+        // console.log('Im running before response is sent out');
+        return plainToClass(UserDto, data, {
+          excludeExtraneousValues: true,
+        });
         
       })
     )
